7883c7183345b8b30873ae1a82d6522446e9369e
Emre Cüni <cuniiemre@gmail.com>
2025-05-23 07:06:03 +0300

    policy service added

---
+++ Customer_RestfulAPI/Models/Policy.cs
@@ -0,0 +1,15 @@
+﻿namespace Customer_RestfulAPI.Models
+{
+    public class Policy
+    {
+        public int Id { get; set; }
+        public string? PolicyNo { get; set; }
+        public int ProductNo { get; set; }
+        public string? Product { get; set; }
+        public DateTime? TransactionDate { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public Customer? Insurer { get; set; }
+        public List<Customer>? InsuredList { get; set; }
+    }
+}
---
+++ Customer_RestfulAPI/Services/PolicyService.cs
@@ -0,0 +1,47 @@
+﻿using Customer_RestfulAPI.Models;
+
+namespace Customer_RestfulAPI.Services
+{
+    public class PolicyService
+    {
+        private readonly List<Policy> _policies = new();
+        private int _id = 1;
+
+        public List<Policy> GetAll() => _policies;
+
+        public Policy? Get(int id) => _policies.FirstOrDefault(p => p.Id == id);
+
+        public Policy Add(Policy policy)
+        {
+            policy.Id = _id++;
+            _policies.Add(policy);
+            return policy;
+        }
+
+        public bool Delete(int id) 
+        {
+            var policy = Get(id);
+            if (policy == null)
+                return false;
+            _policies.Remove(policy);
+            return true;
+        }
+
+        public bool Update(int id, Policy policy)
+        {
+            var existPolicy = Get(id);
+            if(existPolicy == null)
+                return false;
+
+            var props = typeof(Policy).GetProperties();
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(policy);
+                if( value != null) 
+                    prop.SetValue(existPolicy, value);
+            }
+            existPolicy.Id = id;
+            return true;
+        }
+    }
+}
